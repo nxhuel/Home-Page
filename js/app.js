00e0fbb0d7e2c2bc8573d2908407530eaa252b97
nxhuel <nahueltisera03@gmail.com>
2022-10-25 20:38:11 -0300

    Agregando JS funciones a galeria

--- js/app.js
+++ js/app.js
@@ -4,3 +4,16 @@ const navMenu = document.querySelector(".nav-menu")
 navBarra.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible")
 })
+
+
+const fullImgBox = document.getElementById("fullImgBox"),
+fullImg = document.getElementById("fullImg");
+
+function openFullImg(reference) { 
+    fullImgBox.style.display = "flex";
+    fullImg.src = reference
+}
+
+function closeImg() {
+    fullImgBox.style.display = "none";
+}
